c2c1672126c3e00fabde778361ef9006d01885dd
opereal <abolarinhabeeb12@gmail.com>
2024-02-05 15:06:11 +0100

    firstcommit

--- Jsx and components/src/product.js
+++ Jsx and components/src/product.js
@@ -2,6 +2,6 @@ export const product = {
   name: "Wireless Headphones",
   price: 99.99,
   description:
-    "Enjoy immersive sound with these comfortable wireless headphones.",
+    "Enjoy immersive sound with these comfor wireless headphones.",
   imagePath: require("./assets/headphones.webp"),
 };
